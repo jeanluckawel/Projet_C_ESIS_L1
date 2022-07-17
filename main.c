5e3d04960dfb0f80792f1c4eb4043ff7280896cd
jean luc kawel <jeanluckawel45@yahoo.com>
2022-07-18 00:38:13 +0200

    frist commit

--- main.c
+++ main.c
@@ -1,40 +1,40 @@
 #include <stdio.h>
 #include <string.h>
-#define TAILLE 7
+
 typedef struct Personne{
     char nomPersonne[30];
     char telPersonne[10];
 }Personne;
 // Fonction Aquisition
-int Acquisition (Personne*Pers) {
+int Acquisition (Personne Pers[]) {
     printf("Entrer le nom");
     scanf("%s",Pers->nomPersonne);
     printf("Entrer le numero");
     scanf("%s",Pers->telPersonne);
 
-    if (strlen(Pers->nomPersonne)<=30 && strlen(Pers->telPersonne)==10 && Pers->telPersonne[0]=='0' && (Pers->telPersonne[1]=='8' ||  Pers->telPersonne[1]=='9' ))
+    if (strlen(*Pers->nomPersonne)<=30 && strlen(Pers->telPersonne)==10 && Pers->telPersonne[0]=='0' && (Pers->telPersonne[1]=='8' ||  Pers->telPersonne[1]=='9' ))
         return 1;
     else
         return 0;
 }
 //Fonction Ajoute
 void Ajoute(Personne Annuaire[],Personne Pers){
-    int Text=-1;
+    int PasDePersonne=-1;
     int i;
-    for (i = 0; i < TAILLE; i++) {
+    for (i = 0; i < 7; i++) {
         if (strcmp(Annuaire[i].nomPersonne,Pers.nomPersonne)==0)
-            Text=i;
+            PasDePersonne=i;
     }
-    if (Text==-1){
+    if (PasDePersonne==-1){
         Annuaire[0]=Pers;
     }
     else
-        Annuaire [Text]=Pers;
+        Annuaire [PasDePersonne]=Pers;
 }
 // Fonction Affiche
 void Affiche(Personne *Annuaire){
     int i;
-    for (i = 0; i < TAILLE; i++)
+    for (i = 0; i < 7; i++)
     printf("%s NOM : %s TEL\n", Annuaire[i].nomPersonne, Annuaire[i].telPersonne);
 }
 void RechercheTel(Personne Annuaire[]){
@@ -42,7 +42,7 @@ void RechercheTel(Personne Annuaire[]){
     printf("taper le nom");
     scanf("%s",nom);
     int i;
-    for (i = 0; i < TAILLE; i++) {
+    for (i = 0; i < 7; i++) {
         if (strcmp(Annuaire[i].nomPersonne,nom))
             printf("Voici votre : numero %s", Annuaire->telPersonne);
     }
@@ -54,7 +54,7 @@ void RechercheNom(Personne Annuaire[]){
     printf("taper votre nom");
     scanf("%s",tel);
     int i;
-    for (i = 0; i < TAILLE; i++) {
+    for (i = 0; i < 7; i++) {
         if (strcmp(Annuaire[i].telPersonne,tel))
             printf("Voici votre : numero %s", Annuaire->telPersonne);
 
@@ -69,7 +69,7 @@ void RechercheNom(Personne Annuaire[]){
 
 
 int main (void){
-    Personne Annuaire[TAILLE];
+    Personne Annuaire[7];
     printf("##########  Menu Principal  ##########\n1.Ajouter une personne a l'annuaire\n2.fficher le contenu de l'annuaire dans l'ordre alphabétique");
     int choix;
     printf("Taper votre choix------------:>");
