5f20555da6b40a5af7404098ec6074e9ae6afdac
jean luc kawel <jeanluckawel45@yahoo.com>
2022-07-18 16:17:12 +0200

    frist commit

--- main.c
+++ main.c
@@ -2,65 +2,79 @@
 #include <string.h>
 
 typedef struct Personne{
-    char nomPersonne[30];
-    char telPersonne[10];
+    char nomPersonne[31];
+    char telPersonne[11];
 }Personne;
-// Fonction Aquisition
-int Acquisition (Personne Pers[]) {
-    printf("Entrer le nom");
-    scanf("%s",Pers->nomPersonne);
-    printf("Entrer le numero");
-    scanf("%s",Pers->telPersonne);
 
-    if (strlen(*Pers->nomPersonne)<=30 && strlen(Pers->telPersonne)==10 && Pers->telPersonne[0]=='0' && (Pers->telPersonne[1]=='8' ||  Pers->telPersonne[1]=='9' ))
-        return 1;
+// Fonction Aquisition
+int Acquisition (char *nom, char *num) {
+
+    if (strlen(nom)<=30 && strlen(num) == 10 ) {
+        if (num[0] == '0' && (num[1] == '8' || num[1] == '9'))
+            return 1;
+    }
     else
         return 0;
+    return 0;
 }
+
 //Fonction Ajoute
-void Ajoute(Personne Annuaire[],Personne Pers){
-    int PasDePersonne=-1;
-    int i;
-    for (i = 0; i < 7; i++) {
-        if (strcmp(Annuaire[i].nomPersonne,Pers.nomPersonne)==0)
-            PasDePersonne=i;
-    }
-    if (PasDePersonne==-1){
-        Annuaire[0]=Pers;
+void Ajoute(Personne Annuaire[], char *nom, char *numero , int *nombre_personne)
+{
+    if(Acquisition(nom, numero))
+    {
+        strcpy(Annuaire[*nombre_personne].nomPersonne, nom);
+        strcpy(Annuaire[*nombre_personne].telPersonne, numero);
+        *nombre_personne += 1;
+
+        printf("nobre de personne: %d", *nombre_personne);
+        //printf("Nom: %s Num: %s \n", Annuaire[*nombre_personne].nomPersonne, Annuaire[*nombre_personne].telPersonne );
     }
     else
-        Annuaire [PasDePersonne]=Pers;
+        printf("Ajoute impossible ! \n");
 }
+
 // Fonction Affiche
-void Affiche(Personne *Annuaire){
+void Affiche(Personne *Annuaire, const int *nombre_personne){
+
+    for(int i = 0; i < *nombre_personne; i++) {
+
+        if(strlen(Annuaire[i].nomPersonne) > 0)
+            printf("NOM :%s  TEL: %s\n", Annuaire[i].nomPersonne, Annuaire[i].telPersonne);
+        }
+    }
+
+void RechercheTel(Personne Annuaire[], const int *nombre_personne){
+
+    char nomPers[31];
     int i;
-    for (i = 0; i < 7; i++)
-    printf("%s NOM : %s TEL\n", Annuaire[i].nomPersonne, Annuaire[i].telPersonne);
-}
-void RechercheTel(Personne Annuaire[]){
-    char nomPers[30];
-    printf("taper le nom");
+
+    printf("taper le nom    : ");
     scanf("%s",nomPers);
-    int i;
-    for (i = 0; i < 7; i++) {
-        if (strcmp(Annuaire[i].nomPersonne,nomPers))
-            printf("Voici votre : numero %s", Annuaire->telPersonne);
+    for (i = 0; i < *nombre_personne; i++)
+    {
+        if (strcmp(Annuaire[i].nomPersonne,nomPers) == 0)
+            printf("Voici votre : numero %s", Annuaire[i].telPersonne);
     }
 }
 //rechercheNom
 
-void RechercheNom(Personne Annuaire[]){
+void RechercheNom(Personne Annuaire[],const int *nombre_personne){
     char tel[10];
-    printf("taper votre nom");
-    scanf("%s",tel);
-    int i;
-    for (i = 0; i < 7; i++) {
-        if (strcmp(Annuaire[i].telPersonne,tel))
-            printf("Voici votre : numero %s", Annuaire->telPersonne);
 
+    printf("taper votre numeros");
+    scanf("%s", tel);
+
+    int i, pasDeNum=0;
+    for (i = 0; i < *nombre_personne; i++) {
+        if (strcmp(Annuaire[i].telPersonne,tel) == 0)
+        {
+            printf("NOM: %s", Annuaire[i].nomPersonne);
+            pasDeNum++;
+        }
     }
-    int pasDeNum=0;
-    if (pasDeNum != 1){
+
+    if (pasDeNum == 0){
         printf(" pas ce numéro dans l'annuaire!");
     }
 }
@@ -68,16 +82,83 @@ void RechercheNom(Personne Annuaire[]){
 //Efface
 
 
-int main (void){
-    Personne Annuaire[7];
-    printf("##########  Menu Principal  ##########\n1.Ajouter une personne a l'annuaire\n2.fficher le contenu de l'annuaire dans l'ordre alphabétique");
-    int choix;
-    printf("Taper votre choix------------:>");
-    scanf("%d",&choix);
-    switch (choix) {
-        case 1:
-            printf("bonjour");
+void supprimer(Personne *rep, const int *taille, char *nom)
+{
+    int i,j;
+    Personne t={"",0};
+    printf("taper le nom a supprimer");
+    scanf("%s",nom);
+    for ( i = 0; i < *taille; i++)
+    {
+        if (strcmp(rep[i].nomPersonne,nom) == 0)
+        {
+            for ( j = 1; j < *taille; j++)
+            {
+                rep[j] = rep[j + 1];
+            }
+            rep[*taille -1 ] = t;
             break;
+        }
+
     }
+
+
+}
+
+
+int main (void){
+    int nombre_personne = 0;
+    Personne Annuaire[100];
+    int choix = 0 , Programme = 1;
+    char nom[31], num[11];
+
+    while(Programme){
+        printf("\n##########  Menu Principal  ##########\n1.Ajouter l'Annuaire \n "
+               "2.Afficher une personne\n "
+               "3.supprimer une personne\n "
+               "4.Consulter le numero de telephone\n "
+               "5.trouver a qui appartient un certain numero\n "
+               "6. sortir du programme\n");
+
+        printf("Taper votre choix   :");
+        scanf("%d",&choix);
+
+        switch (choix) {
+            case 1:
+                printf("\n#######[IDENTIFIANT]#######\n");
+
+                printf("Nom\t:");
+                scanf("%s",nom);
+
+                printf("Numeros\t:");
+                scanf("%s",num);
+
+                Ajoute(Annuaire, nom, num, &nombre_personne);
+
+                break;
+
+            case 2:
+                Affiche(Annuaire, &nombre_personne);
+                break;
+            case 3:
+
+                supprimer(Annuaire,&nombre_personne,nom);
+
+                break;
+            case 4:
+                RechercheTel(Annuaire,&nombre_personne);
+                break;
+            case 5:
+                RechercheNom(Annuaire,&nombre_personne);
+                break;
+            case 6:
+                printf("Fin de la partie :)");
+                Programme = 0 ;
+                break;
+            default:
+                printf("Mauvaise entree!");
+        }
+    }
+
     return 0;
 }
